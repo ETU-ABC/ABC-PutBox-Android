0da341decde6893350d2dbbe63b380de73275961
burakdmb <burakhan.demirbilek@gmail.com>
2018-11-03 21:17:59 +0300

    Added the feature to hardware back button to act like the webview back

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
+import {
+  BackHandler,
+  Platform,
+  WebView
+} from 'react-native';
+
+var DEFAULT_URL = 'https://lit-sierra-86142.herokuapp.com/';
 
 export default class App extends Component {
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
+
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
   render() {
     return (
+
       <WebView
-        source={{uri: 'https://lit-sierra-86142.herokuapp.com/'}}
         style={{marginTop: 20}}
+        ref={(webView) => { this.webView.ref = webView; }}
+        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+        source={{uri: DEFAULT_URL}}
       />
+
     );
   }
+
 }
