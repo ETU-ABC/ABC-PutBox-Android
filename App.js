a1fc4bbaa4b12fc6e58e7464278b57e64a102fe2
burakdmb <burakhan.demirbilek@gmail.com>
2018-11-27 19:46:06 +0300

    Updated the project, implemented notification feature for #8

--- App.js
+++ App.js
@@ -1,18 +1,102 @@
-import React, { Component } from 'react';
-import {
-  BackHandler,
-  Platform,
-  WebView
-} from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView, WebView, BackHandler } from 'react-native';
+
+import firebase from 'react-native-firebase';
+import { Notification, NotificationOpen } from 'react-native-firebase';
+import Cookie from 'react-native-cookie';
+
 
 var DEFAULT_URL = 'http://putbox-abc.herokuapp.com/';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      url: DEFAULT_URL
+    };
+  }
 
-export default class App extends Component {
+  async componentDidMount() {
+    if (Platform.OS === 'android') {
+     BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+
+
+    firebase.notifications().getInitialNotification()
+      .then((notificationOpen) => {
+        if (notificationOpen) {
+          // App was opened by a notification
+          // Get the action triggered by the notification being opened
+          const action = notificationOpen.action;
+          // Get information about the notification that was opened
+          const notification = notificationOpen.notification;
+          console.log("Opened notification: ",notification);
+          this.changeURL(notification.data.url);
+          console.log("Changed the url to ",notification.data.url);
+        }
+      });
+    this.cookieCheck();
+  }
+  cookieCheck(){
+    const user = firebase.auth().currentUser;
+    Cookie.get('http://putbox-abc.herokuapp.com/','token').then((cookie) =>{
+      if(user){
+        if(cookie!==null && cookie!==undefined){
+          if(user.email!==cookie.concat('@abc.com').toLowerCase()){
+            console.log("Logging out(cookie and email are different) from user: ",cookie.concat('@abc.com'), user.email);
+            firebase.messaging().unsubscribeFromTopic(user.email.substring(0, user.email.lastIndexOf("@")));
+            firebase.auth().signOut().then(()=>{
+              firebase.auth()
+              .signInWithEmailAndPassword(cookie.concat('@abc.com'),'123456')
+              .catch(error => {
+                  console.log(error.message, error.code);
+                  if(error.code=='auth/user-not-found'){
+                    firebase.auth().createUserWithEmailAndPassword(cookie.concat('@abc.com'),'123456')
+                    .catch(error =>{
+                      console.log(error.message, error.code);
+                    });
+                  }
+              })
+              .then(()=>{
+                firebase.messaging().subscribeToTopic(cookie).then(()=>{
+                  console.log("Logged out(cookie and email are different) and relogged&subbed to: ",cookie);
+                  console.log('Logged user info1-> ', user.toJSON());
+                }).catch(error =>{
+                  console.log(error.message, error.code);
+                });
+              });
+           });
+         }
+       }
+      }
+      else{
+        if(cookie!==null && cookie!==undefined){
+          firebase.auth()
+          .signInWithEmailAndPassword(cookie.concat('@abc.com'),'123456')
+          .catch(error => {
+            console.log(error.message, error.code);
+              if(error.code=='auth/user-not-found'){
+                firebase.auth().createUserWithEmailAndPassword(cookie.concat('@abc.com'),'123456')
+                .catch(error =>{
+                  console.log(error.message, error.code);
+                });
+              }
+          });
+          firebase.messaging().subscribeToTopic(cookie).then(()=>{
+            console.log('No user exists before and logged in to cookie: ', cookie)
+            console.log('Logged user info2-> ', user.toJSON());
+          }).catch(error =>{
+            console.log(error.message, error.code);
+          });
+        }
+      }
+    }).catch(error=>{
+      console.log(error.message, error.code);
+    });
+  }
   webView = {
     canGoBack: false,
     ref: null,
   }
-
   onAndroidBackPress = () => {
     if (this.webView.canGoBack && this.webView.ref) {
       this.webView.ref.goBack();
@@ -20,29 +104,28 @@ export default class App extends Component {
     }
     return false;
   }
-
-  componentWillMount() {
-    if (Platform.OS === 'android') {
-      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
-    }
-  }
-
   componentWillUnmount() {
     if (Platform.OS === 'android') {
       BackHandler.removeEventListener('hardwareBackPress');
     }
+    this.cookieCheck();
+  }
+  changeURL(newUrl){
+    console.log(newUrl);
+    this.setState({ url: newUrl });
   }
   render() {
+    const { url } = this.state;
     return (
-
       <WebView
         style={{marginTop: 20}}
         ref={(webView) => { this.webView.ref = webView; }}
-        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
-        source={{uri: DEFAULT_URL}}
+        onNavigationStateChange={(navState) => {
+          this.webView.canGoBack = navState.canGoBack;
+        ;}}
+        source={{uri: url}}
       />
 
     );
   }
-
 }
--- README.md
+++ README.md
@@ -4,17 +4,12 @@ This is a React Native App for the PutBox application.
 
 This applications uses React Native and Expo to run in Android & IOS
 
-To run, please follow these steps : 
+To run, please follow these steps :
 1. Clone the project.
 2. Install npm, 'sudo apt install npm'
-3. Install Expo Command Line Interface, 'sudo npm install -g expo-cli'
+3. Install android studio and tools for android app development
 4. Change your directory into the PutBox folder, 'cd PutBox/'
-5. Start Expo, 'expo start'
-6. Install the Expo app from the Google Play Store or App Store
-7. After that you can scan the qr code generated in your computers terminal to run the PutBox mobile app from your mobile device.
+5. run 'npm run android-linux' to run this app in a device
+6. run 'npm run android-linux-release' to export a signed apk
 
-Expo application links:
-
-https://play.google.com/store/apps/details?id=host.exp.exponent
-
-https://itunes.apple.com/app/apple-store/id982107779
+PutBox mobile app from your mobile device.
